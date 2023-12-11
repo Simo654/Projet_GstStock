ebb1b5a40365566af57eb38efb1adf31cfbd831f
Simo654 <Elmouhsinemohammed@outlook.fr>
2023-12-11 10:13:56 +0100

    "Oumy"

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -24,6 +24,7 @@ const Dashboard = () => {
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
       </Box>
+      sjbdjks
 
       {/* GRID & CHARTS */}
       <Box
